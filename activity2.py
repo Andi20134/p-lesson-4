6280161f09366b4a369b746a7ab717af09fb0cec
Andi15034 <agunung1503@gmail>
2026-02-14 07:11:36 -0500

    ATM

---
+++ activity2.py
@@ -0,0 +1,12 @@
+
+Amount =int(input("Please Enter  Amountfor withdraw :"))
+
+
+note_1 = Amount//100
+note_2 = (Amount%100)//50
+note_3 = ((Amount%100)%50)//10
+
+
+print("Number of 100 dollars ",note_1)
+print("Number of 50 dollars",note_2)
+print("Number of 10 dollars ",note_3)
